0f6d040e36ee108197c962cfeceee280d786158a
Izanaaagi <vlad.rotar2@gmail.com>
2021-05-13 03:26:50 +0300

    add /cat, /dog commands and functionality

--- bot.js
+++ bot.js
@@ -1,16 +1,35 @@
 require('dotenv').config()
-const {Telegraf} = require('telegraf')
+const TelegramBot = require('node-telegram-bot-api')
+const axios = require('axios');
 
-const bot = new Telegraf(process.env.TELEGRAM_TOKEN)
-bot.start(ctx => {
-    ctx.reply(
-        'Это я'
-    )
+const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, {polling: true})
+const catURI = 'https://aws.random.cat/meow'
+const dogURI = 'https://dog.ceo/api/breeds/image/random'
+
+
+bot.setMyCommands([
+    {command: '/start', description: 'Start working with bot'},
+    {command: '/cat', description: 'Get random cat'},
+    {command: '/dog', description: 'Get random dog'},
+])
+
+bot.on('message', async msg => {
+    const chatId = msg.chat.id
+
+    if (msg.text === '/cat') {
+        axios.get(catURI)
+            .then(resp => {
+                const photoURI = resp.data.file
+                bot.sendPhoto(chatId, photoURI)
+            })
+    }
+
+    if (msg.text === '/dog') {
+        axios.get(dogURI)
+            .then(resp => {
+                const photoURI = resp.data.message
+                bot.sendPhoto(chatId, photoURI)
+            })
+    }
+
 })
-
-bot.command('cat', ctx => {
-    console.log(ctx.message)
-    ctx.replyWithPhoto(`https://http.cat/100`)
-})
-
-bot.launch()
